5df3371005f992fddc749db8095768a618967c19
Nim Wijetunga <nimw@snaptravel.com>
2020-05-02 11:21:50 -0400

    modify brew

--- brew.sh
+++ brew.sh
@@ -41,6 +41,7 @@ brew install gnupg
 # Install more recent versions of some macOS tools.
 brew install vim --with-override-system-vi
 brew cask install keepingyouawake
+brew cask install visual-studio-code
 brew install grep
 brew install openssh
 brew install screen
